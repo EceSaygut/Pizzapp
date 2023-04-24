f2e4a17541f80c8aa987cdb6aec74bada35f6da4
EceSaygut <49751847+EceSaygut@users.noreply.github.com>
2023-04-25 00:02:26 +0300

    Initial Commit

---
+++ Pizzapp/PizzappApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzappApp.swift
+//  Pizzapp
+//
+//  Created by Ece Saygut on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
